3b4258370b32a8b6934f88c8e125d50119d66f10
mu <semkadarov@gmail.com>
2024-04-19 22:09:50 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task1");
+        int[] moneyInBook = {1, 2, 3, 4, 5};
+        int summInMonth = 0;
+        for (int i : moneyInBook) {
+            summInMonth += i;
+        }
+        System.out.println("Сумма трат за месяц составила " + summInMonth + " рублей");
 
-    }
-}
