a231962a4153b9fb124ed9fe65e36b2a2901b611
mu <semkadarov@gmail.com>
2024-04-20 21:07:28 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,25 @@ public class Main {
 
         System.out.println("Task2");
         int[] moneyInBookSecond = {816, 895, 865, 878, 825};
-        int minWaste = -1;
+        int minWaste = 999999999;
         int maxWaste = -1;
         for (int i = 0; i < 5; i++) {
             if (moneyInBookSecond[i] > maxWaste) {
                 maxWaste = moneyInBookSecond[i];
-            } else {
+            }
+            if (moneyInBookSecond[i] < minWaste) {
                 minWaste = moneyInBookSecond[i];
             }
         }
         System.out.printf("Минимальная сумма трат за неделю составила %s рублей. Максимальная сумма трат за неделю составила %s рублей %n", minWaste, maxWaste);
+
+        System.out.println("Task3");
+        int[] moneyInBookThird = {51, 74, 12, 73, 64};
+        double midSummInMonth = 0;
+        for (int i = 0; i < moneyInBookThird.length; i++) {
+            midSummInMonth += moneyInBookThird[i];
+        }
+        midSummInMonth = midSummInMonth / moneyInBookThird.length;
+        System.out.printf("Средняя сумма трат за месяц составила %s рублей%n", midSummInMonth);
     }
 }
